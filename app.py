7328921762a20c432aa0ff0cbada11f5fd6e7c58
shaumikkhanna <shaumikkhanna@gmail.com>
2024-06-27 14:57:40 +0530

    simplified game logic

--- app.py
+++ app.py
@@ -20,14 +20,11 @@ def computer_move():
     node_states = request.json['node_states']
     difficulty = float(request.json['difficulty'])
 
-    move = compute_move(node_states, 1 - difficulty)
+    star = Starnim(node_states=node_states)
+    move = star.next_move_node(1 - difficulty)
+
     return jsonify({'move': move})
 
-
-def compute_move(node_states, error_probability):
-    star = Starnim(node_states=node_states)
-    move = star.next_move_node(error_probability)
-    return move
 
 if __name__ == '__main__':
     app.run(port=8000)
